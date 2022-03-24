6897cc3f765c2d2249d18102c1f6309471f4628c
Yurim <yurimplus@naver.com>
2022-03-24 23:59:29 +0900

    [브루트 포스] 3월 24일 - Update

--- Class5/2503.cpp
+++ Class5/2503.cpp
@@ -4,42 +4,32 @@
 
 using namespace std;
 const int MAX = 1001;
+vector<bool> correct_numbers(MAX, true); //전체 배열을 true로 설정
 
-int check(int number, int strike, int ball) {
+void check(int i, int number, int strike, int ball) {
     string check_num = to_string(number);
     int strike_cnt = 0;
     int ball_cnt = 0;
-    vector<bool> correct_numbers(MAX, false);
-
-    for (int i = 123; i < 988; i++) {
-        string original_num = to_string(i);
-        for (int a = 0; a < 3; a++) {
-            if (original_num[a] == '0') { //자리에 0이 있으면 false
+    string original_num = to_string(i);
+    for (int a = 0; a < 3; a++) {
+        if (original_num[a] == '0') { //자리에 0이 있으면 false
+            correct_numbers[i] = false;
+        }
+        for (int b = 0; b < 3; b++) {
+            if (original_num[a] == original_num[b]) { //숫자가 서로 같은 경우는 제외
                 correct_numbers[i] = false;
             }
-            if (original_num[a] == check_num[a]) { //자리랑 숫자 모두 같을 때
+            if (a == b && original_num[b] == check_num[a]) { //자리랑 숫자 모두 같을 때
                 strike_cnt++;
+            } else if (original_num[b] == check_num[a]) {//자리는 다르지만 같은 숫자가 존재할 때
+                ball_cnt++;
             }
-            for (int b = 0; b < 3; b++) {
-                if (a != b && original_num[b] == check_num[a]) {//자리는 다르지만 같은 숫자가 존재할 때
-                    ball_cnt++;
-                }
-            }
+        }
 
-        }
-        if (strike_cnt == strike && ball_cnt == ball) {
-            correct_numbers[i] = true;
-        }
     }
-
-    int count = 0;
-    for (int i = 0; i < MAX; i++) {
-        if (correct_numbers[i]) {
-            count++;
-            cout << i << '\n';
-        }
+    if (strike_cnt != strike || ball_cnt != ball) { //strike, ball 모두 개수가 맞으면 맞는 숫자임
+        correct_numbers[i] = false;
     }
-    return count;
 }
 
 int main() {
@@ -48,7 +38,19 @@ int main() {
     while (n--) {
         int number, strike, ball;
         cin >> number >> strike >> ball;
-        cout << check(number, strike, ball);
+        for (int i = 123; i < 988; i++) {
+            if (correct_numbers[i]) {
+                check(i, number, strike, ball);
+            }
+        }
     }
+
+    int count = 0;
+    for (int k = 123; k < 988; k++) { //true인 숫자들만 count증가, 출력
+        if (correct_numbers[k]) {
+            count++;
+        }
+    }
+    cout << count << '\n';
     return 0;
 }
