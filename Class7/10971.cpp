578ce777efc3bdc58b1ca769c0c90c04f7d5f27b
Yurim <yurimplus@naver.com>
2022-04-01 15:06:30 +0900

    [백트래킹] 4월 일 - Update

--- Class7/10971.cpp
+++ Class7/10971.cpp
@@ -2,50 +2,57 @@
 #include <vector>
 
 using namespace std;
-int n;
-const int MAX = 11;
-vector<vector<int>> W;
-int min_cost = 999999999;
-vector<bool> check(MAX, false);
+const int INF = 1e8; //최대 크기 상수로 저장
 
-void backtracking(int start, int count, int cost) {
-    if (cost >= min_cost) { //비용이 최소비용보다 크면 답이 아니므로 리턴
-        return;
+vector<bool> visited; //방문 검사 배열
+vector<vector<int>> matrix; //비용 행렬
+int n, ans = INF; //최소 비용 저장 위해 미리 최댓값으로 초기화
+
+/**
+ * !생각해보기! 함수의 각 인자는 각각 어떤 역할을 하고 있나요? //cnt: 인덱스, cur_city:현재 도시, cost: 비용
+ */
+void backtracking(int cnt, int cur_city, int cost) { //백트래킹 함수
+    if (cost >= ans) { //생각해보기 : 이 조건문이 없으면 어떻게 될까요? 현재 비용이 현재의 최소 비용보다 크면 답이 되지 못하므로
+        return; //리턴
     }
-    if (count == n) { //모두 순회했을 경우
-        for (int i = 0; i < n; i++) { //행 체크
-            if (W[start][i] != 0) {
-                min_cost = min(cost, min_cost); //최소 비용 구하기
-            }
+    if (cnt == n) { //종료 조건 : n개의 도시를 순회했음
+        //출발 도시(0)로 다시 돌아올 수 있는지 확인
+        if (matrix[cur_city][0] != 0) { //갈 수 있는 도시라면
+            ans = min(ans, cost + matrix[cur_city][0]); //현재 최소비용과 현재 도시와 직전의 비용을 합친 비용 중 더 작은 비용을 최소 비용에 저장한다
         }
-        return;
+        return; //리턴
     }
-
-    for (int i = 0; i < n; i++) { //행 체크
-        if (W[start][i] != 0 && !check[i]) { //갈 수 있는 도시이면서 들리지 않은 도시라면
-            check[i] = true; //들렸다고 체크
-            cost += W[start][i]; //비용 더하기
-            backtracking(start, count + 1, cost); //다음 도시 체크
-            check[i] = false; //들리지 않은 것으로 되돌리기
+    for (int i = 0; i < n; i++) { //cur_city에서 도시 i로 이동
+        if (matrix[cur_city][i] && !visited[i]) { //길이 있고, 아직 방문하지 않은 도시
+            visited[i] = true; //방문 표시해줌
+            backtracking(cnt + 1, i, cost + matrix[cur_city][i]); //백트래킹 다음 인덱스 탐색
+            visited[i] = false; //다시 방문 표시 되돌려놓음
         }
     }
 }
 
+/**
+ * 백트래킹 풀이 (4ms)
+ *
+ * 한 사이클을 만들면, 어느 도시에서 시작하든 값은 같으므로 0번 도시부터 시작하는 경우만 검사하면 된다.
+ */
+
 int main() {
-    cin >> n;
-    W.assign(n, vector<int>(n, 0));
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            cin >> W[i][j];
+    //입력
+    cin >> n; //입력받기
+    visited.assign(n, false); //방문 배열의 크기를 n으로, 초기값을 false로 초기화
+    matrix.assign(n, vector<int>(n, 0)); //매트릭스 배열을 크기가 n이고 초기값이 0인 벡터로 초기화
+    for (int i = 0; i < n; i++) { //n만큼 박복하면서
+        for (int j = 0; j < n; j++) { //n만큼 반복하면서
+            cin >> matrix[i][j]; //전체 도시의 비용을 입력받는다.
         }
     }
-    for (int i = 0; i < n; i++) {//0번행부터 시작
-        check[i] = true;
-        backtracking(i, 0, 0);
-        check[i] = false;
 
-    }
-    cout << min_cost << '\n';
-    return 0;
+    //연산
+    visited[0] = true; //시작지점 방문 표시
+    backtracking(1, 0, 0); //백트래킹 함수 호출
 
+    //출력
+    cout << ans; //최소비용 출력
+    return 0;  //리턴
 }
