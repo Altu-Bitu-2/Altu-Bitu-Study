ffc5d486bbcc920addece272a7d155ff94c4c35a
Yurim <yurimplus@naver.com>
2022-04-14 21:58:47 +0900

    [이분탐색 ] 4월 14일 - Update

--- Class11/16401.cpp
+++ Class11/16401.cpp
@@ -7,7 +7,7 @@ using namespace std;
 void binarySearch(vector<int> &l, int m, int n) {
     int start = 1; //1부터 시작
     int end = l[n - 1]; //가장 큰 수가 end가 됨
-    int answer; //정답
+    int answer = 0; //정답
     while (start <= end) { //end가 start보다 크거나 같을 때까지
         int count = 0;
         int mid = (start + end) / 2; //중간값 정해주기
@@ -15,7 +15,7 @@ void binarySearch(vector<int> &l, int m, int n) {
             count += l[i] / mid; //mid길이로 나눈 과자의 개수
         }
         if (count >= m) { //과자 개수가 m보다 크거나 같으면
-            if(answer < mid){
+            if (answer < mid) {
                 answer = mid;
             }
             start = mid + 1;
